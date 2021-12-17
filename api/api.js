4ef22875e6607ad307896782477c0666889d0ac9
Valentin <valentin@elephantslab.com>
2021-12-17 14:28:48 +0200

    add getTodaysOrders endpoint, parsed metadata to fit standarts

--- api/api.js
+++ api/api.js
@@ -27,6 +27,20 @@ const connection = mysql.createConnection({
 
 const PORT = 3000;
 
+const places = [
+
+    {"NAME": "Restaurant 1", "ID": 0, "TABLES": 10, "IMG": ""},
+    {"NAME": "Restaurant 2", "ID": 1, "TABLES": 12, "IMG": ""},
+    {"NAME": "Restaurant 3", "ID": 2, "TABLES": 9, "IMG": ""},
+    {"NAME": "Restaurant 4", "ID": 3, "TABLES": 7, "IMG": ""},
+    {"NAME": "Restaurant 5", "ID": 4, "TABLES": 20, "IMG": ""},
+    {"NAME": "Restaurant 6", "ID": 5, "TABLES": 13, "IMG": ""},
+    {"NAME": "Restaurant 7", "ID": 6, "TABLES": 15, "IMG": ""},
+    {"NAME": "Restaurant 8", "ID": 7, "TABLES": 10, "IMG": ""},
+    {"NAME": "Restaurant 9", "ID": 8, "TABLES": 12, "IMG": ""},
+    {"NAME": "Restaurant 10", "ID": 9, "TABLES": 14,"IMG": ""}
+]
+
 https.createServer({
     key: fs.readFileSync('../../../../../etc/letsencrypt/live/aurorabooking.net/privkey.pem'),
     cert: fs.readFileSync('../../../../../etc/letsencrypt/live/aurorabooking.net/cert.pem')
@@ -40,7 +54,17 @@ app.get('/metadata/:id', async function(req, res){
         const NFT_id = req.params.id;
         const result = await getMetadataByNftId(NFT_id);
         
-        return res.send(result[0]);
+        return res.send(result);
+    } catch (error) {
+        console.log(error)
+        return res.status(404).send();
+    }
+})
+app.get('/getTodaysOrders', async function(req, res){
+    try {
+        const result = await getTodaysOrders();
+        
+        return res.send(result);
     } catch (error) {
         console.log(error)
         return res.status(404).send();
@@ -156,14 +180,24 @@ async function getMetadataByNftId(nftId){
     if(!nftId) {
         return;
     }
+    let result;
     const timestamp = Math.floor(new Date() / 1000)
     const currentDate = timeConverter(timestamp);
     let getMetadataQuery = `SELECT * from table_orders where nft_id="${nftId}" and date ="${currentDate}"`;
     let getMetadataQueryResult = await connection.query(getMetadataQuery);
     if(getMetadataQueryResult[0].length){
-        getMetadataQueryResult[0][0]["image"] = `${baseURL}/getQr/${nftId}`;
+        result = parseMetadate(getMetadataQueryResult[0][0]);
     }
-    return getMetadataQueryResult[0];
+    console.log(result)
+    return result;
+}
+
+async function getTodaysOrders(){
+    const timestamp = Math.floor(new Date() / 1000)
+    const currentDate = timeConverter(timestamp);
+    let query = `SELECT * from table_orders where date = "${currentDate}"`;
+    const qeuryResult = await connection.query(query);
+    return qeuryResult[0]
 }
 
 async function setNewBooking(nft_id, place_id, table_number, date) {
@@ -205,3 +239,29 @@ const generateQR = async text => {
       console.error(err)
     }
   }
+
+function parseMetadate(metadate) {
+    console.dir(metadate)
+    const place = places.find(el => el.ID === metadate.place_id);
+    const resObj ={
+        "name": `Your booking in ${place.NAME}, table: #${metadate.table_number}`,
+        "image": `${baseURL}/getQr/${metadate.nft_id}`,
+        "description": "This is your order confirmation",
+        "atributes":[
+            {
+                "trait_type":`place_id`,
+                "value":`${metadate.place_id}`,
+            },
+            {
+                "trait_type":`table_number`,
+                "value":`${metadate.table_number}`,
+            },
+            {
+                "trait_type":`nft_id`,
+                "value":`${metadate.nft_id}`,
+            }
+        ]
+    }
+
+    return resObj
+}
